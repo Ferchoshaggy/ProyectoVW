658f17ee75c2791b7f548a558efdcba7c0787c9d
unknown <fernandoshaggy562@gmail.com>
2022-10-20 15:21:52 -0500

    import

---
+++ app/Imports/InventoryImport.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\inventory;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+
+class InventoryImport implements ToModel, WithHeadingRow
+{
+    /**
+    * @param array $row
+    *
+    * @return \Illuminate\Database\Eloquent\Model|null
+    */
+    public function model(array $row)
+    {
+        return new inventory([
+            //
+        ]);
+    }
+}
--- app/Models/inventory.php
+++ app/Models/inventory.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class inventory extends Model
 {
     use HasFactory;
+
+    protected $guarded=[];
 }
