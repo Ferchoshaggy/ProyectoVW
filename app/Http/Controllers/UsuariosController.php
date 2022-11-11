2b319f73b43ed5aea00668d9d3df9bada01b2b39
unknown <fernandoshaggy562@gmail.com>
2022-11-11 17:59:09 -0600

    final

--- app/Http/Controllers/UsuariosController.php
+++ app/Http/Controllers/UsuariosController.php
@@ -44,10 +44,10 @@ class UsuariosController extends Controller
         "password"=>bcrypt($request['contraseña']),
 
         ]);
-/*
+
         $data=["name"=>$request['nombre'] ,"email"=>$request['correo2'],"password"=>$request['contraseña'],"empresa"=>$request['concesionaria']];
         Mail::to($request['correo'])->send(new MessageReceived("Usuario Creado",$data,"users"));
-*/
+
         return redirect()->back()->with(['message' => 'Usuario Guardado con Éxito', 'color' => 'success']);
 
     } catch (\Exception $e) {
