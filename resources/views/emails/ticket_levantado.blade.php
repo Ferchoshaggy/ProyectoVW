28dd96f4b1661a4a870fb10bdc0a5a6dae132759
unknown <fernandoshaggy562@gmail.com>
2022-12-14 12:06:21 -0600

    css

--- resources/views/emails/ticket_levantado.blade.php
+++ resources/views/emails/ticket_levantado.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ticket Levantado</title>
 </head>
-<body>
+<body style="background-color: #E8E7E2; padding-top: 10px; padding-bottom: 10px;">
 
     <div style="margin-right: 15%; margin-left: 15%; margin-top: 10px; margin-bottom:10px; border-radius: 10px; background-color: white;">
 
@@ -15,7 +15,7 @@
     @if($datos['empresa']=="Navarra") src="{{url('img/imgemail/EncaNM.png')}}" @endif
    style="width: 100%; height: auto; border-radius: 15px;">
 
-   <div style="margin-top: 100px;margin-bottom: 100px; text-align: center;font-size:15px">
+   <div style="margin-top: 100px;margin-bottom: 100px; text-align: center;font-size:20px">
  <h4>Alguien a levantado un ticket</h4>
  El usuario {{$datos['name']}} Ha levantado un ticket el dia {{$datos['fecha']}}<br>
    </div>
